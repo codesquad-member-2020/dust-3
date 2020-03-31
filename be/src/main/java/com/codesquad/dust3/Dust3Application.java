16c800b9f664b03c490669ab28ac0533667b471e
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-03-31 15:10:16 +0900

    [be] chore: 프로젝트 생성

---
+++ be/src/main/java/com/codesquad/dust3/Dust3Application.java
@@ -0,0 +1,13 @@
+package com.codesquad.dust3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dust3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dust3Application.class, args);
+	}
+
+}
---
+++ be/src/test/java/com/codesquad/dust3/Dust3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.dust3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dust3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
