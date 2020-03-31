a224330b671f2a047911a6613620cf557b13d806
David Chung <david215@berkeley.edu>
2020-03-31 18:05:19 +0900

    [be] feat: forecast-info mock API 구현
    - URL: forecast-info
    - 24시간 mock 데이터를 JSON 형식에 맞춰 반환.
    - e.g. forecastInfo = {
               forecastImage: "52.78.203.80:8080/img-0.png",
               generalForecast: "[미세먼지] 전 권역이 '좋음'∼'보통'으로 예상됨...",
               airQualityIndexByRegion: "서울 : 보통,제주 : 좋음, ..."
           }

--- be/src/main/java/com/codesquad/dust3/AirQualityController.java
+++ be/src/main/java/com/codesquad/dust3/AirQualityController.java
@@ -51,7 +51,15 @@ public class AirQualityController {
     }
 
     @GetMapping("/forecast-info")
-    public void getForecastInfo() {
-
+    public ResponseEntity<List<ForecastInfo>> getForecastInfo() {
+        List<ForecastInfo> forecastInfos = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            ForecastInfo forecastInfo = new ForecastInfo();
+            forecastInfo.setGeneralForecast("[미세먼지] 전 권역이 '좋음'∼'보통'으로 예상됨. 다만, 서울·경기도·충청권은 오전에 일시적으로 '나쁨' 수준일 것으로 예상됨.");
+            forecastInfo.setAirQualityIndexByRegion("서울 : 보통,제주 : 좋음,전남 : 보통,전북 : 보통,광주 : 보통,경남 : 좋음,경북 : 좋음,울산 : 좋음,대구 : 좋음,부산 : 좋음,충남 : 보통,충북 : 보통,세종 : 보통,대전 : 보통,영동 : 좋음,영서 : 보통,경기남부 : 보통,경기북부 : 보통,인천 : 보통");
+            forecastInfo.setForecastImage("52.78.203.80:8080/test-" + i + ".png");
+            forecastInfos.add(forecastInfo);
+        }
+        return ResponseEntity.ok(forecastInfos);
     }
 }
