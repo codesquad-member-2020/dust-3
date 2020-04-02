efa060e2f4c99962acc2cb6c5b4f8e7f6e7bb10b
GangWoon <gangwoon2018@gmail.com>
2020-04-02 22:06:25 +0900

    [iOS] add: FineDustViewModel 생성
    FineDustView에서 사용하는 그라데이션 컬러를 만들어주는 객체를 생성했습니다.

---
+++ ios/Dust/Dust/FineDust/FineDustViewModel.swift
@@ -0,0 +1,63 @@
+//
+//  FineDustViewModel.swift
+//  Dust
+//
+//  Created by Cloud on 2020/04/02.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+class FineDustViewModel {
+    
+    enum Colors {
+        case happy
+        case normal
+        case mask
+        case scream
+        
+        init(_ status: String) {
+            switch status {
+            case "좋음":
+                self = .happy
+            case "보통":
+                self = .normal
+            case "나쁨":
+                self = .mask
+            default:
+                self = .scream
+            }
+        }
+        
+        func generateColor() -> [CGColor] {
+            switch self {
+            case .happy:
+                return [
+                    UIColor.systemIndigo.cgColor,
+                    UIColor.systemTeal.cgColor,
+                ]
+            case .normal:
+                return [
+                    UIColor.systemGreen.cgColor,
+                    UIColor.secondarySystemFill.cgColor,
+                ]
+            case .mask:
+                return [
+                    UIColor.systemOrange.cgColor,
+                    UIColor.systemYellow.cgColor,
+                    UIColor.systemBackground.cgColor,
+                ]
+            default:
+                return [
+                    UIColor.systemRed.cgColor,
+                    UIColor.systemPink.cgColor,
+                    UIColor.systemYellow.cgColor,
+                ]
+            }
+        }
+    }
+    
+    func generateGradientColor(status: String) -> [CGColor] {
+        Colors(status).generateColor()
+    }
+}
