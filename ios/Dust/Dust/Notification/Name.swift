57f1cf4ecd4fec744d9473dfcb4720f2516459f0
GangWoon <gangwoon2018@gmail.com>
2020-04-02 21:54:01 +0900

    [iOS] add: 노티피케이션 네임 생성
    노티피케이션 센터를 사용할 때 필요한 이름들을 전역 변수로 먼저 생성해놨습니다.

---
+++ ios/Dust/Dust/Notification/Name.swift
@@ -0,0 +1,18 @@
+//
+//  Name.swift
+//  Dust
+//
+//  Created by Cloud on 2020/04/02.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static var updateStatusView: Notification.Name = Notification.Name("updateStatusView")
+    static var pushLoacation: Notification.Name = Notification.Name("pushLoacation")
+    static var pushAirQualityInfos: Notification.Name = Notification.Name("pushAirQualityInfos")
+    static var reloadTableView: Notification.Name = Notification.Name("reloadTableView")
+    static var updateColor: Notification.Name = Notification.Name("updateColor")
+    static var firstCell: Notification.Name = Notification.Name("firstCell")
+}
