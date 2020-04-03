ea27a914efe175af6551a174d973d648fd31f4b9
Hu2y <sanghun_@daum.net>
2020-04-03 12:26:25 +0900

    [fe] add: forecastPlayComponent 작성
     - 이미지 재생버튼을 위한 템플릿 코드 작성

---
+++ fe/src/components/ForecastPlayComponent.js
@@ -0,0 +1,17 @@
+class ForecastPlayComponent {
+  constructor() {
+
+  }
+
+  render() {
+    return `
+      <div class="gage">
+        <button class="play-btn">▶</button>
+        <div class="bar"></div>
+        <button class="play-status" style="left:0px">🐶</button>
+      </div>
+    `
+  }
+}
+
+export default ForecastPlayComponent
