05537f64fafaf48aaf882616999abae47b227bdd
GangWoon <gangwoon2018@gmail.com>
2020-04-03 09:51:44 +0900

    [iOS] refactor: FineDustViewController 수정
    tableview delegate를 처리할 객체를 생성하고 싶었습니다. 하지만 시간적인 부분에 의해서 viewController에서 처리하도록 했습니다.

--- ios/Dust/Dust/FineDust/FineDustViewController.swift
+++ ios/Dust/Dust/FineDust/FineDustViewController.swift
@@ -10,4 +10,73 @@ import UIKit
 
 class FineDustViewController: UIViewController {
     
+    // MARK: - Properties
+    private var fineDustView: FineDustView! = FineDustView()
+    private var fineDustTableViewDataSource = FineDustTableViewDataSource()
+    private var networkManager: NetworkManager = NetworkManager()
+    private var locationManager: LocationManager = LocationManager()
+    
+    // MARK: - Lifecycle
+    override func loadView() {
+        view = fineDustView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fineDustView.tableView.dataSource = fineDustTableViewDataSource
+        fineDustView.tableView.delegate = self
+        addObserver()
+    }
+    
+    deinit {
+        removeObserver()
+    }
+    
+    // MARK: - Methods
+    private func addObserver() {
+        NotificationCenter.default
+            .addObserver(
+                self,
+                selector: #selector(updateView),
+                name: Notification.Name.pushAirQualityInfos,
+                object: nil
+        )
+    }
+    
+    private func removeObserver() {
+        NotificationCenter.default
+        .removeObserver(
+            self,
+            name: Notification.Name.pushAirQualityInfos,
+            object: nil
+        )
+    }
+    
+    // MARK: - @objc
+    @objc func updateView(_ notification: Notification) {
+        guard let airQualityInfos = notification.userInfo?.first?.value
+            as? [AirQualityInfos.AirQualityInfo] else { return }
+        fineDustTableViewDataSource.updateInfos(airQualityInfos)
+    }
+}
+
+extension FineDustViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.cellHeight
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let tableview = scrollView as! UITableView
+        guard let aa = tableview.visibleCells.first, let ads = tableview.indexPath(for: aa)?.item else { return }
+        NotificationCenter.default
+            .post(
+                name: Notification.Name.firstCell,
+                object: nil,
+                userInfo: ["index": ads]
+        )
+    }
+}
+
+extension UITableView {
+    static var cellHeight: CGFloat  = 22
 }
