78703a8ca44870a9d1949c3c46208ae2a269d8d9
Hu2y <sanghun_@daum.net>
2020-04-03 12:40:26 +0900

    [fe] chore: 서버로 데이터 받기 전 목데이터 테스트

---
+++ fe/src/data/mockData.js
@@ -0,0 +1,172 @@
+const mockData = [
+  {
+    airQualityIndex: "1",
+    PM10: "15",
+    currentTime: "2016-04-20 14:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "2",
+    PM10: "40",
+    currentTime: "2016-04-20 13:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "2",
+    PM10: "45",
+    currentTime: "2016-04-20 12:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "1",
+    PM10: "12",
+    currentTime: "2016-04-20 11:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "2",
+    PM10: "30",
+    currentTime: "2016-04-20 10:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "3",
+    PM10: "70",
+    currentTime: "2016-04-20 09:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "4",
+    PM10: "75",
+    currentTime: "2016-04-20 08:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "4",
+    PM10: "90",
+    currentTime: "2016-04-20 07:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "3",
+    PM10: "55",
+    currentTime: "2016-04-20 06:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "3",
+    PM10: "62",
+    currentTime: "2016-04-20 05:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "3",
+    PM10: "45",
+    currentTime: "2016-04-20 04:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "3",
+    PM10: "55",
+    currentTime: "2016-04-20 03:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "2",
+    PM10: "39",
+    currentTime: "2016-04-20 02:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "1",
+    PM10: "13",
+    currentTime: "2016-04-20 01:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "1",
+    PM10: "15",
+    currentTime: "2016-04-20 14:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "2",
+    PM10: "40",
+    currentTime: "2016-04-20 13:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "2",
+    PM10: "45",
+    currentTime: "2016-04-20 12:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "1",
+    PM10: "12",
+    currentTime: "2016-04-20 11:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "2",
+    PM10: "30",
+    currentTime: "2016-04-20 10:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "3",
+    PM10: "70",
+    currentTime: "2016-04-20 09:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "4",
+    PM10: "75",
+    currentTime: "2016-04-20 08:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "4",
+    PM10: "90",
+    currentTime: "2016-04-20 07:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "3",
+    PM10: "55",
+    currentTime: "2016-04-20 06:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+  {
+    airQualityIndex: "3",
+    PM10: "62",
+    currentTime: "2016-04-20 05:00",
+    location: "종로구",
+    color: '#00b6f5'
+  },
+]
+
+export default mockData
---
+++ fe/src/data/mockData3.js
@@ -0,0 +1,148 @@
+const airQualityInfos = [
+  {
+    "currentTime": "2020-04-02 20:00",
+    "pm10": 41,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-02 19:00",
+    "pm10": 45,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-02 18:00",
+    "pm10": 48,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-02 17:00",
+    "pm10": 50,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-02 16:00",
+    "pm10": 49,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-02 15:00",
+    "pm10": 63,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-02 14:00",
+    "pm10": 55,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-02 13:00",
+    "pm10": 48,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-02 12:00",
+    "pm10": 45,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-02 11:00",
+    "pm10": 37,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-02 10:00",
+    "pm10": 36,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-02 09:00",
+    "pm10": 31,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-02 08:00",
+    "pm10": 30,
+    "location": "송파구",
+    "airQualityIndex": "좋음"
+  },
+  {
+    "currentTime": "2020-04-02 07:00",
+    "pm10": 29,
+    "location": "송파구",
+    "airQualityIndex": "좋음"
+  },
+  {
+    "currentTime": "2020-04-02 06:00",
+    "pm10": 25,
+    "location": "송파구",
+    "airQualityIndex": "좋음"
+  },
+  {
+    "currentTime": "2020-04-02 05:00",
+    "pm10": 26,
+    "location": "송파구",
+    "airQualityIndex": "좋음"
+  },
+  {
+    "currentTime": "2020-04-02 04:00",
+    "pm10": 22,
+    "location": "송파구",
+    "airQualityIndex": "좋음"
+  },
+  {
+    "currentTime": "2020-04-02 03:00",
+    "pm10": 22,
+    "location": "송파구",
+    "airQualityIndex": "좋음"
+  },
+  {
+    "currentTime": "2020-04-02 02:00",
+    "pm10": 22,
+    "location": "송파구",
+    "airQualityIndex": "좋음"
+  },
+  {
+    "currentTime": "2020-04-02 01:00",
+    "pm10": 22,
+    "location": "송파구",
+    "airQualityIndex": "좋음"
+  },
+  {
+    "currentTime": "2020-04-01 24:00",
+    "pm10": 23,
+    "location": "송파구",
+    "airQualityIndex": "좋음"
+  },
+  {
+    "currentTime": "2020-04-01 23:00",
+    "pm10": 25,
+    "location": "송파구",
+    "airQualityIndex": "좋음"
+  },
+  {
+    "currentTime": "2020-04-01 22:00",
+    "pm10": 32,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  },
+  {
+    "currentTime": "2020-04-01 21:00",
+    "pm10": 48,
+    "location": "송파구",
+    "airQualityIndex": "보통"
+  }
+]
+
+export default airQualityInfos
